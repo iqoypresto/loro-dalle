3a3370149438fcfeb17bd26e7312089a1033804a
iqoypresto <rfqimaulana888@gmail.com>
2023-08-23 00:17:05 +0800

    adding admin sided-navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import { Link } from "react-router-dom"
-import { FaBars, FaTimes } from "react-icons/fa"
+import { Link, NavLink } from "react-router-dom"
+import { FaBars, FaTimes, FaUsers, FaCoins } from "react-icons/fa"
 import { IoNotificationsOutline } from "react-icons/io5"
 import { CgProfile } from "react-icons/cg"
 import { useState } from "react";
+import {LiaHomeSolid} from "react-icons/lia"
+import {GiSwapBag} from "react-icons/gi"
 
 export function Navbar() {
 
@@ -51,6 +53,7 @@ export function DashboardNavbar() {
 
     const [isOpen, setIsOpen] = useState(false);
     const [isTransparent, setIsTransparent] = useState(false)
+    const [isProfile, setIsProfile] = useState(false)
 
     const changeOpacity = () => {
         if (window.scrollY >= 100) {
@@ -64,9 +67,14 @@ export function DashboardNavbar() {
         setIsOpen(!isOpen)
     }
 
+    const handleProfileClick = () => {
+        setIsProfile(!isProfile)
+    }
+
     window.addEventListener('scroll', changeOpacity)
     return (
         <>
+
             <nav className={isTransparent ? "transparent" : ""}>
                 <div className="nav-content">
                     <h1>Loro Dalle'</h1>
@@ -87,15 +95,52 @@ export function DashboardNavbar() {
                                     <IoNotificationsOutline size={20} />
                                 </Link>
                             </li>
-                            <li>
-                                <Link>
-                                    <CgProfile size={48} />
-                                </Link>
+                            <li className="relative">
+                                <a className="hover:cursor-pointer">
+                                    <CgProfile size={48} onClick={handleProfileClick} />
+                                </a>
+                                <div className={`profile-setting ${isProfile ? "drop-shadow-2xl z-10" : "hidden"}`}>
+                                    <p className="mb-3">Anto Bukanmain</p>
+                                    <p className="font-light text-xs">anto@gmail.com</p>
+                                    <div className="menu mt-3 gap-y-2 flex flex-col">
+                                        <Link className="py-1">Profile Setting</Link>
+                                        <Link to="/" className="py-1">Log Out</Link>
+                                    </div>
+                                </div>
                             </li>
                         </div>
                     </ul>
                 </div>
             </nav>
+        </>
+    )
+}
+
+export const AdminNavbar = () => {
+    return (
+        <>
+            <div className="admin-navbar">
+                <Link to="/admin-dashboard" className="logo">LORO' DALLE</Link>
+                <ul>
+                    <div className="point">
+                        <li>
+                            <NavLink to="/admin-dashboard"> <LiaHomeSolid size={18} className="me-9" />Dashboard</NavLink>
+                        </li>
+                    </div>
+                    <div className="point">
+                        <p className="header">User Section</p>
+                        <li>
+                            <NavLink to="/admin-user-section"> <FaUsers size={18} className="me-9" />Users</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/"> <GiSwapBag size={18} className="me-9" />Sampah</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/"> <FaCoins size={18} className="me-9" />Point</NavLink>
+                        </li>
+                    </div>
+                </ul>
+            </div>
         </>
     )
 }
